669f7d1f6280bced6684df4de6c6ea9f999fc306
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-11-15 01:39:30 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,9 @@
-import { ThreeDots, BallTriangle } from 'react-loader-spinner';
+import { BallTriangle } from 'react-loader-spinner';
 import { CenterBox } from './Loader.styled';
 
 export const Loader = () => {
   return (
     <CenterBox>
-      {/* <ThreeDots
-        height="80"
-        width="80"
-        radius="9"
-        color="blue"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClassName=""
-        visible={true}
-      /> */}
-
       <BallTriangle
         heigth="100"
         width="100"
